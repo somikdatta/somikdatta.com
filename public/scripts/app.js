88fe9eb15d117d8dff946777c4aa12f3c2c8b72d
Somik Datta <developersomik@gmail.com>
2020-04-06 19:43:21 +0530

    Build folder generated

---
+++ public/scripts/app.js
@@ -0,0 +1,4 @@
+var downloadResume = () => {
+    window.open('./assets/somik_resume.pdf', '_blank');
+}
+console.log("Hi Geek,\nI love to be friends with like minded people.\nMessage me, maybe we could help each other.\nCheers,\nSomik Datta")
