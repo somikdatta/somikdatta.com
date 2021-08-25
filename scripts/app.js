e90849794f25168a99b573c2a1c0b7024460081e
Somik Datta <developersomik@gmail.com>
2021-08-25 21:53:08 +0530

    Update: Resume

--- scripts/app.js
+++
@@ -1,4 +0,0 @@
-var downloadResume = () => {
-    window.open('./assets/somik_resume.pdf', '_blank');
-}
-console.log("Hi Geek,\nI love to be friends with like minded people.\nMessage me, maybe we could help each other.\nCheers,\nSomik Datta")
